95cd6868116b0d2d82406289452234cbfae68543
Nikos Karamessinis <csd3335@csd.uoc.gr>
2018-12-28 21:26:32 +0200

    Basic Exceptions

---
+++ Exceptions/BasicExceptions.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void mightGoWrong() {
+	bool error1 = false;
+	bool error2 = true;
+	if (error1) {
+		throw "Something went wrong";
+	}
+	if (error2) {
+		throw string("Something else went wrong");
+	}
+}
+
+void usesMightGoWrong() { mightGoWrong(); }
+
+int main() {
+	try {
+		usesMightGoWrong();
+	}
+	catch (int e) {
+		cout << "Error code: " << e << endl;;
+	}
+	catch (char const *e) {
+	
+		cout << "Error message: " << e << endl;
+	}
+	catch (string &e) {
+		cout << "String Error message " << e << endl;
+	}
+	cout << "still running" << endl;
+	while (true);
+	return 0;
+
+}
