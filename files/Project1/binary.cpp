3555e04f7027739c920468bc2c9ce947506414cd
Nikos Karamessinis <csd3335@csd.uoc.gr>
2018-12-29 23:59:31 +0200

    Reading and Writing binary

--- files/Project1/binary.cpp
+++ files/Project1/binary.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 using namespace std;
 #pragma pack(push,1)
 
 struct Person {
-	char c;
 	char name[50];
 	int age;
 	double weight;
@@ -15,7 +15,46 @@ struct Person {
 #pragma pack(pop)
 
 int main() {
-	cout << sizeof(Person) << endl;
+
+	Person someone = {"Frodo",220,0.8};
+	string fileName = "test.bin";
+
+
+	// Write binary file
+	ofstream outputFile;
+	outputFile.open(fileName, ios::binary);
+	if (outputFile.is_open()) {
+
+		
+		
+		//outputFile.write((char *)&someone,sizeof(someone));
+		outputFile.write(reinterpret_cast<char *>(&someone), sizeof(someone));
+
+
+		outputFile.close();
+	}
+	else {
+		cout << "Could not create file" + fileName;
+	}
+
+
+	Person someoneElse = {};
+	//Read File 
+	ifstream inputFile;
+	inputFile.open(fileName, ios::binary);
+	if (inputFile.is_open()) {
+
+
+
+		//outputFile.write((char *)&someone,sizeof(someone));
+		inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(someone));
+
+
+		inputFile.close();
+	}
+	else {
+		cout << "Could not create file" + fileName;
+	}
+	cout << someoneElse.name<<","<<someoneElse.age<<","<<someoneElse.weight;
 	while (1);
-	return 0;
 }
