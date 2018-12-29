87e374db40899fa1173e996b4f1900fdeae174a5
Nikos Karamessinis <csd3335@csd.uoc.gr>
2018-12-29 23:15:16 +0200

    Parsing text files

---
+++ files/Project1/parse.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+	string filename = "stats.txt";
+	ifstream input;
+	input.open(filename);
+	if (!input.is_open()) {
+		return 1;
+	}
+	while (input) {
+		string line;
+		getline(input, line, ':');
+		
+		int population;
+		if (!input) {
+			break;
+		}
+		input >> population;
+		
+		input >> ws;
+		
+		
+		cout << line << "-----" << population << endl;
+	}
+	input.close();
+	return 0;
+}
