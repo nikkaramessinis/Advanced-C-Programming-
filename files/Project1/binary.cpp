e8c41c49709a879491a22614df5bedcd39adbc11
Nikos Karamessinis <csd3335@csd.uoc.gr>
2018-12-29 23:29:57 +0200

    Pragma pack

---
+++ files/Project1/binary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+#pragma pack(push,1)
+
+struct Person {
+	char c;
+	char name[50];
+	int age;
+	double weight;
+
+
+};
+
+#pragma pack(pop)
+
+int main() {
+	cout << sizeof(Person) << endl;
+	while (1);
+	return 0;
+}
