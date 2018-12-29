cce930c037d7371c25499fa32243e15c8c71c599
Nikos Karamessinis <csd3335@csd.uoc.gr>
2018-12-29 11:38:33 +0200

    Standard Exceptions

---
+++ Standard Exceptions/BasicExceptions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CanGoWrong {
+public:
+	CanGoWrong() {
+		char* pMemory = new char[999999];
+		delete[]pMemory;
+	
+	}
+
+
+};
+
+int main() {
+
+	try {
+		CanGoWrong wrong;
+	}
+	catch (bad_alloc & e) {
+		cout << "Caught exception "<<e.what() << endl;
+	}
+	return 0;
+
+}
