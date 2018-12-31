7202eb9a0f4088cbabff87d2c76203565670c0c4
Nikos Karamessinis <nikkaramessinis@github.com>
2018-12-31 12:57:17 +0200

    Multimaps

---
+++ STL/STL/Multimaps.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+class Person {
+private:
+	int age;
+	string name;
+public:
+	Person(string name, int age) :name(name), age(age) {}
+
+	void print() const {
+		cout << name << ":" << age << endl;
+	}
+	bool operator<(const Person &other) const {
+		if (name == other.name) { return age < other.age; }
+		else { return name < other.name; }
+	}
+
+
+};
+int main() {
+
+	multimap<int, string> lookup;
+	lookup.insert(make_pair(30, "Mike"));
+	lookup.insert(make_pair(10, "Vicky"));
+	lookup.insert(make_pair(30, "Raj"));
+	lookup.insert(make_pair(20, "Bob"));
+		for (multimap<int, string>::iterator it = lookup.begin(); it != lookup.end(); it++){
+			cout << it->first << ":" << it->second << endl;
+		}
+		cout << endl;
+		for (multimap<int, string>::iterator it = lookup.find(20); it != lookup.end(); it++) {
+			cout << it->first << ":" << it->second << endl;
+		}
+		cout << endl;
+		pair<multimap<int, string>::iterator, multimap<int, string>::iterator> its = lookup.equal_range(30);
+
+		for (multimap<int, string>::iterator it = its.first; it != its.second; it++) {
+			cout << it->first << ":" << it->second << endl;
+		}
+
+
+		cout << endl;
+		auto its2 = lookup.equal_range(30);
+
+		for (multimap<int, string>::iterator it = its2.first; it != its2.second; it++) {
+			cout << it->first << ":" << it->second << endl;
+		}
+		while (1);
+}
