9e8fe4cd58280eb5716887d093409eb4d1609aaa
Nikos Karamessinis <nikkaramessinis@github.com>
2019-01-09 02:32:59 +0200

    Type interfere

--- Templates/Templates/Templates.cpp
+++ Templates/Templates/Templates.cpp
@@ -18,14 +18,28 @@ public:
 };
 template<class T>
 void  print(T n) {
+	cout << "Template version" << endl;
 	cout << n << endl;
+}
+
+void print(int value) {
+	cout << "Non template version" << endl;
+	cout << value << endl;
+}
+
+
+template<class T>
+void show() {
+	cout << T() << endl;
 }
 int main() {
 	Test<string, int>test1("Hello");
 	test1.print();
 	print<string>("hello");
 	print<int>(5);
-
+	print(6);
+	print<>(6); 
+	show<int>();
 	while (1);
 	return 0;
 }
