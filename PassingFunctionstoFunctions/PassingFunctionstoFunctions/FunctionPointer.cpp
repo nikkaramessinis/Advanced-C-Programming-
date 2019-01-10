a83c4bbe58981ddaba76834ca47025193adee481
Nikos Karamessinis <nikkaramessinis@github.com>
2019-01-10 21:38:12 +0200

    Passing a function to a pointer

---
+++ PassingFunctionstoFunctions/PassingFunctionstoFunctions/FunctionPointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void test(int value) {
+	cout << "Hello" <<value<< endl;
+}
+
+int main() {
+	test(3);
+	void (*pTest)(int)=test;
+
+	//pTest = test;
+	pTest(4);
+
+	while (1);
+	return 0;
+}
