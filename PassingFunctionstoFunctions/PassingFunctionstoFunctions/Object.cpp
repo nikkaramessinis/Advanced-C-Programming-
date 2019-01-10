c3c0421607602a40fe35f73f24a2c6e18132ecd0
Nikos Karamessinis <nikkaramessinis@github.com>
2019-01-10 23:36:03 +0200

    Polymorphism and Slicing

---
+++ PassingFunctionstoFunctions/PassingFunctionstoFunctions/Object.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class Parent {
+private:
+	int one;
+public:
+	Parent():one(0) {}
+	Parent(const Parent& other):one(0) {
+		cout << "Copy parent" << endl;
+		one = other.one;
+	}
+	virtual void print() {
+		cout << "parent" << endl;
+	}
+
+	virtual ~Parent() {}
+};
+
+
+class Child :public Parent {
+private:
+	int two;
+public:
+	Child() :two(0) {
+	
+	}
+	void print() {
+		cout << "child" << endl;
+	}
+
+};
+
+
+int main() {
+	Child c1;
+	Parent &p1 = c1;
+	p1.print();
+
+	Parent p2 = Child();
+	p2.print();
+
+	while (1);
+}
