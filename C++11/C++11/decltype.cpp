93ea5d96cf80f8693060c8f4b26fc25349b1fc39
Nikos Karamessinis <nikkaramessinis@github.com>
2019-01-11 21:04:18 +0200

    decltype and typeid

---
+++ C++11/C++11/decltype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <typeinfo>
+#include <string>
+using namespace std;
+
+int main() {
+	string value;
+
+	cout << typeid(value).name();
+
+	decltype(value) name="Bob";
+	cout << typeid(name).name()<<endl;
+	cout << name;
+
+	while (1);
+}
